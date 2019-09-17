d0f0a8295a7a76687896736f192dd023170e5d61
nvandenberge <nvandenberge14@gmail.com>
2019-09-17 13:26:50 -0400

    function to add a task

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
-const taskInput = document.querySelector('#tasks');
+const taskInput = document.querySelector('#task');
 
 // Load Event Listeners
 loadEventListener();
@@ -16,7 +16,17 @@ function loadEventListener(){
 // Add Task
 function addTask(e){
     if(taskInput.value === '') {
-        alert('Add a task')
+        alert('Add a task');
     }
+
+    // Create list element
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(taskInput.value));  //Creates list item with text
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class = "fa fa-remove"></i>';
+    li.appendChild(link);
+    taskList.appendChild(li);
     e.preventDefault();
 }
